813bb4643718dee182dc55fd69dc222769284c18
Daniel Smith <daniel@smitbo.com>
2017-07-22 20:18:46 -0400

    Fixed issue where stardate was always reported as an integral value. Now it should properly display with 2 decimal places.

--- stardate-calculator/stardate.c
+++ stardate-calculator/stardate.c
@@ -9,14 +9,14 @@ int main(int argc, char** argv)
    if(argc == 1)
    {
       long long now = (long long) time(NULL);
-      float stardate = (now - STARDATE_0) / 31536;
+      float stardate = (now - STARDATE_0) / 31536.0;
       printf("The current stardate is %.2f\n\n", stardate);
       return 0;
    }
    else if(argc == 2)
    {
       long long in = atoll(argv[1]);
-      float stardate = (in - STARDATE_0) / 31536;
+      float stardate = (in - STARDATE_0) / 31536.0;
       printf("At the provided UNIX timestamp, the stardate will be / was %.2f\n\n", stardate);
       return 0;
    }
